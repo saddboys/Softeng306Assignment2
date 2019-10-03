228b68a179aa2a3332722520b5b58154b3f5ef7b
Chuyang Chen <cdavidnz@gmail.com>
2019-10-03 13:52:12 +1300

    camera utils

---
+++ Game/Assets/CameraDrag.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+ 
+public class CameraDrag : MonoBehaviour
+{
+    public float dragSpeed;
+    private Vector3 dragOrigin;
+ 
+ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+ 
+        Vector2 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector2 move = new Vector2(-pos.x * dragSpeed, -pos.y * dragSpeed);
+        transform.Translate(move, Space.World);
+    }
+ 
+ 
+}

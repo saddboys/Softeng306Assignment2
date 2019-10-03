908acf341ec74bda4ab5567f848d82ee1fcfc6cc
mliu433 <maggieqliu81@gmail.com>
2019-10-03 16:28:20 +1300

    add rock strcuture button select method

--- Game/Assets/Scripts/ToolBar.cs
+++ Game/Assets/Scripts/ToolBar.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Game.CityMap;
 namespace Game
 {
     public class ToolBar : MonoBehaviour
@@ -20,13 +20,51 @@ namespace Game
         // Start is called before the first frame update
         void Start()
         {
-
+        //load structure items dynamically, assume static here
+        //Populate();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+        
         }
+
+        //initialise list of structures on the toolbar
+        void Populate(){
+ 
+        }
+
+        //trigger button select event 
+        void OnSelect(){
+        //check if the button is clickable 
+        StructureFactory sf = new RockFactory();
+        
+        if(sf.CanBuild(out string reason)){
+        //only one component now for testing use
+        GameObject rockBtn = GameObject.FindWithTag("RockButton");
+        string tag = "RockButton";
+        Debug.Log("find the object with tag" + tag);
+        //create a new rock 
+        Rock rock = (RockFactory)sf.Create();
+        
+        }else{
+        //deselect the buttons 
+        //TODO: now deselect all
+        Selection.activeGameObject = null;
+        }
+        }
+
+        //call method in StructureFactory create a specified strcuture component
+        // void CreateStructure(string tag){
+        
+        // }
+
+        //add the strcuture component to the grid at a specified position
+        void ClickGrid(){
+        
+        }
+
+
     }
 }

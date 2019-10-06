5f033ed5267944eaa3f1773b8c5ca01f8f76dc63
mliu433 <maggieqliu81@gmail.com>
2019-10-06 13:14:23 +1300

    clean comments, add rm button function to unity and c#

--- Game/Assets/Scripts/ToolBar.cs
+++ Game/Assets/Scripts/ToolBar.cs
@@ -11,16 +11,12 @@ namespace Game
         [SerializeField] private City city;
         [SerializeField] private Toggle toggle ;
         private Rock rock;
-        private bool btnSelected;
+        private bool rockBtnSelected, rmBtnSelected;
 
         public ToolBar(City city) { }
 
         // Start is called before the first frame update
         void Start() {
-        //  toggle = GetComponent<Toggle>();
-        //  toggle.onValueChanged.AddListener(OnToggleValueChanged);
-
-            // E.g.
             city.Map.TileClickedEvent += (s, e) =>
             {
                 // TODO: handle when the tile e.Tile has been clicked.
@@ -37,31 +33,9 @@ namespace Game
         
         }
 
-        //initialise list of structures on the toolbar
-        // void Populate(){
- 
-        // }
-
-        //trigger button select event 
-        // void OnSelect(){
-        // //check if the button is clickable 
-        // StructureFactory sf = new RockFactory();
-        
-        // if(sf.CanBuild(out string reason)){
-        // //only one component now for testing use
-        // GameObject rockBtn = GameObject.FindWithTag("RockButton");
-        // string tag = "RockButton";
-        // Debug.Log("find the object with tag" + tag);
-        
-        // }else{
-        // //deselect the buttons 
-        // GameObject btn = GameObject.Find("RockButton").GetComponent<GameObject>();
-        // }
-        // }
-
         void OnNotify(MapTile tile) {
             //check if one of the toggle is selected
-            if (btnSelected) {
+            if (rockBtnSelected) {
                 //check if the tile is free
                 if(tile.Structure == null){
                 tile.Structure = new Rock();
@@ -71,23 +45,19 @@ namespace Game
                 //tile.Terrain.Sprite = Resources.LoadAll<Sprite>("Textures/terrain")[28]; 
                 // StructureFactory factory = new RockFactory(city);
                 // factory.BuildOnto(tile);
+            }else if(rmBtnSelected){
+                 tile.Structure = null;
             }
 
         }
 
-        //call method in StructureFactory create a specified strcuture component
-        // void CreateStructure(string tag){
-        
-        // }
-
-        //add the strcuture component to the grid at a specified position
-        // void ClickGrid(){
-        
-        // }
-
     // Called whenever something is toggled on
      public void OnToggleValueChanged( bool isOn ) {
-         btnSelected = isOn;
+         rockBtnSelected = isOn;
+     }
+
+      public void OnRmToggleValueChanged( bool isOn ) {
+         rmBtnSelected = isOn;
      }
 
     }

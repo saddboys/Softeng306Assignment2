89a2b0aa5383c424f3a8ff78333fee3674c5bd65
MelancholicGhost <jkim584@aucklanduni.ac.nz>
2019-10-07 23:47:47 +1300

    Implemented more park request

---
+++ Game/Assets/Scripts/Story/Events/MoreParkRequest.cs
@@ -0,0 +1,62 @@
+﻿using Game.CityMap;
+using UnityEngine;
+
+namespace Game.Story.Events
+{
+    public class MoreParkRequest : StoryRequest
+    {
+        public override string Title
+        {
+            get { return TITLE; }
+        }
+
+        public override string Description
+        {
+            get { return DESCRIPTION; }
+        }
+
+        public override Sprite EventImage
+        {
+            get { return SPRITE; }
+        }
+
+        public override ToolBar ToolBar
+        {
+            get { return toolBar; }
+            set
+            {
+                toolBar = value;
+                toolBar.BuiltEvent += OnBuild;
+            }
+        }
+        
+        private ToolBar toolBar;
+
+        private const string TITLE = "More Park Request";
+        private const string DESCRIPTION = "You are growing in population. Build a public park.";
+        private const Sprite SPRITE = null;
+
+        public override void OnYesClick()
+        {
+            ToolBar.gameObject.SetActive(false);
+            EndButton.interactable = false;
+            ToolBar.CurrentFactory = new ParkFactory();
+        }
+        
+        
+
+        private void OnBuild()
+        {
+            ToolBar.gameObject.SetActive(true);
+            EndButton.interactable = true;
+            ToolBar.CurrentFactory = null;
+            ToolBar.BuiltEvent -= OnBuild;
+            
+            
+        }
+        public override void OnNoClick()
+        {
+            City.Stats.Reputation -= 0.75;
+        }
+    }
+}

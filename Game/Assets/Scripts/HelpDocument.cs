0215c347eebd53f74b5c74f3c5aff7734bf30736
mliu433 <maggieqliu81@gmail.com>
2019-10-07 21:11:13 +1300

    add help document and click event to the newly created button on game scene called Help

---
+++ Game/Assets/Scripts/HelpDocument.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Story
+{
+public class HelpDocument : MonoBehaviour
+{   [SerializeField]
+    private GameObject newCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    // Canvas c = newCanvas.AddComponent<Canvas>();
+    // c.renderMode = RenderMode.ScreenSpaceOverlay;
+    //  newCanvas.AddComponent<CanvasScaler>();
+    //  newCanvas.AddComponent<GraphicRaycaster>();
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+    GameObject panel = new GameObject("Panel");
+     panel.AddComponent<CanvasRenderer>();
+     panel.
+     Image i = panel.AddComponent<Image>();
+     i.color = Color.red;
+     panel.transform.SetParent(newCanvas.transform, false);
+    }
+}
+}

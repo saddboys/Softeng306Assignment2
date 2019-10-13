87a9e16062323ab19057f9d3da5e64ee1a6d9dd9
mliu433 <maggieqliu81@gmail.com>
2019-10-14 10:22:08 +1300

    add all untracked files

---
+++ Game/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Game
+{
+[System.Serializable]
+public class Dialogue 
+{
+    public string name;
+    [TextArea(3,10)]
+    public string[] sentences;
+}
+}
---
+++ Game/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Game
+{
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public Queue<string> sentences;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+       animator.SetBool("isOpen", true);
+        Debug.Log("start conversation!"+ dialogue.name);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+            if(sentences.Count == 0){
+                EndDialogue();
+                return;
+            }
+           
+           string sentence = sentences.Dequeue();
+            Debug.Log("next conversation!"+ sentence);
+           dialogueText.text = sentence;
+           StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+    public void EndDialogue(){
+      animator.SetBool("isOpen", false);
+    }
+
+
+}
+}

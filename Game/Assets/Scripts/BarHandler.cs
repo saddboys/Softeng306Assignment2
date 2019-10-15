c465271cdb492d953a1bd497e277762398657e97
mliu433 <maggieqliu81@gmail.com>
2019-10-15 14:33:37 +1300

    make temperature bar work properly

---
+++ Game/Assets/Scripts/BarHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Game
+{
+public class BarHandler : MonoBehaviour
+{   [SerializeField]
+    public Slider slider;
+    private double currentValue = 0.0;
+    public GameObject bar;
+    [SerializeField]
+    private StatsBar stats;
+    public double CurrentValue {
+    get {
+        return currentValue;
+    }
+    set {
+        currentValue = (float)stats.Temperature;
+        bar = GameObject.Find("TemperatureBar");
+        slider.value =(float)currentValue;
+       
+    }
+}
+
+void Update(){
+    slider.value =(float)stats.Temperature;
+}
+ 
+}
+}

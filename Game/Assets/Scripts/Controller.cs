29faaa91f7c53ad39ded0e7a044fb19413a4c0eb
Ernest Wong <ewon521@gmail.com>
2019-10-16 19:45:49 +1300

    Implement exit button for editor and WebGL

--- Game/Assets/Scripts/Controller.cs
+++ Game/Assets/Scripts/Controller.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game
 {
@@ -12,9 +14,37 @@ namespace Game
         [SerializeField]
         private GameObject statsBar;
 
+        // This links to the JS function inside the plugins folder.
+        [DllImport("__Internal")]
+        private static extern void WebGLExit();
+
         public void Quit()
         {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
             Application.Quit();
+#elif UNITY_WEBGL
+            // Restart the game.
+            // Note: Button may not exist (not active) if quiting outside of menu.
+            GameObject
+                .Find("Restart Button")
+                ?.GetComponent<Button>()
+                ?.onClick.Invoke();
+
+            // Exit fullscreen.
+            Screen.fullScreen = false;
+
+            // Show html exit screen.
+            WebGLExit();
+#endif
+        }
+
+        private void Start()
+        {
+#if UNITY_WEBGL
+            Screen.fullScreen = true;
+#endif
         }
 
         private void Update()

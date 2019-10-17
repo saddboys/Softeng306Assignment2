4a15a611c61ca72414bc5df70f9be89ef413bec3
tnzhu <tzhu442@aucklanduni.ac.nz>
2019-10-17 22:41:09 +1300

    Added script for text colour change effect when button is hovered over

---
+++ Game/Assets/Scripts/UI/ButtonHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Text ButtonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ButtonText.color = Color.white;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonText.color = Color.black;
+    }
+}

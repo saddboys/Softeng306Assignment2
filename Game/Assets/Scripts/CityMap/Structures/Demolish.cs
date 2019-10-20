5f313130861d9bf4141f9b7a8e485b5dcdf87876
AXon <seismiccat@protonmail.com>
2019-10-21 12:14:38 +1300

    fix String.Format index

--- Game/Assets/Scripts/CityMap/Structures/Demolish.cs
+++ Game/Assets/Scripts/CityMap/Structures/Demolish.cs
@@ -54,7 +54,7 @@ namespace Game.CityMap
             int popRequirement = -tile.Structure.GetStatsChangeOnDemolish().Population;
             if (City.Stats.Population <= popRequirement)
             {
-                reason = String.Format("Cannot demolish when you have less than {1}k people", popRequirement);
+                reason = String.Format("Cannot demolish when you have less than {0}k people", popRequirement);
                 return false;
             }
 
